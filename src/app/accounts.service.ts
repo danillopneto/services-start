d2d7e1291d34f2a2f25dd1cb363ed80a50fd545f
Danillo Pinheiro Neto <danillopneto@outlook.com>
2022-03-14 22:05:45 -0300

    first commit

---
+++ src/app/accounts.service.ts
@@ -0,0 +1,36 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { LoggingService } from "./logging.service";
+
+@Injectable({providedIn: 'root'})
+export class AccountsService {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+  statusUpdated = new EventEmitter<string>();
+  
+  constructor(private loggingService: LoggingService){
+  }
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({
+      name: name,
+      status: status
+    });
+  }
+
+  updateStatus(id: number, newStatus: string) {
+    this.accounts[id].status = newStatus;
+    this.loggingService.logStatusChange(newStatus);
+  }
+}
